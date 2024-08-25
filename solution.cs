e6aad924e78e019fcc8f5f47d3440b4216e3cf86
Kooreis <kooreis.official@gmail.com>
2024-08-25 11:28:09 -0500

    This is the function that places the dominos on the board. It iterates over the board and places a domino in the first available spot it finds, either horizontally or vertically.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple JavaScript console application that solves the problem of placing dominos on a board without overlaps. This solution assumes that the board is a 2D array and a domino is represented as a 2x1 or 1x2 array. The program will print the board with the placed dominos.
-
-```javascript
 function placeDominos(board, domino) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
@@ -19,27 +16,3 @@ function placeDominos(board, domino) {
     }
     return false;
 }
-
-function printBoard(board) {
-    for (let i = 0; i < board.length; i++) {
-        console.log(board[i].join(' '));
-    }
-}
-
-let board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-];
-
-let domino = 1;
-
-while (placeDominos(board, domino)) {
-    printBoard(board);
-    console.log('\n');
-    domino++;
-}
-```
-
-This program will place dominos on the board from left to right, top to bottom. When a domino is placed, the function `placeDominos` will return `true` and the board will be printed. The domino number will then be incremented. The program will stop when there are no more places to put a domino, at which point `placeDominos` will return `false`.
