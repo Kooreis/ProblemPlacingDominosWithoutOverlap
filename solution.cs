2b9f618ca1777397db9fb1d5bf7cb80c1589a37b
Kooreis <kooreis.official@gmail.com>
2024-08-26 11:28:09 -0500

    This function prints the current state of the board. It iterates over the board and prints each row.

--- solution.cs
+++ solution.cs
@@ -1,18 +1,5 @@
-function placeDominos(board, domino) {
+function printBoard(board) {
     for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j] === 0) {
-                if (j + 1 < board[i].length && board[i][j + 1] === 0) {
-                    board[i][j] = domino;
-                    board[i][j + 1] = domino;
-                    return true;
-                } else if (i + 1 < board.length && board[i + 1][j] === 0) {
-                    board[i][j] = domino;
-                    board[i + 1][j] = domino;
-                    return true;
-                }
-            }
-        }
+        console.log(board[i].join(' '));
     }
-    return false;
 }
